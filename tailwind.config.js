e017086ffcbd06ae0670d6c141601349d9bd0cdb
Tomasz <tomasz.iwanicki@onet.pl>
2023-06-06 19:31:16 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        mainPurple: '#635FC7',
+        lightPurple: '#A8A4FF',
+        black: '#000112',
+        veryDarkGray: '#20212C',
+        darkGray: '#2B2C37',
+        gray: '#3E3F4E',
+        mediumGray: '#828FA3',
+        lightGray: '#E4EBFA',
+        veryLightGray: '#F4F7FD',
+        red: '#EA5555',
+        lightRed: '#FF9898',
+      },
+    },
+  },
+  plugins: [],
+}
