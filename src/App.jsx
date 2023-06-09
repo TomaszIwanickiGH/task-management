2f00df88ba524f0694e7c180ca64451707741cc5
Tomasz <tomasz.iwanicki@onet.pl>
2023-06-09 20:06:28 +0200

    Import new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import Navbar from './components/Navbar'
+import Header from './components/Header'
+import Main from './components/Main'
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <div className="flex w-full">
+      <Navbar />
+      <div className="w-full bg-veryDarkGray overflow-hidden">
+        <Header />
+        <Main />
+      </div>
+    </div>
+  )
 }
 
 export default App
