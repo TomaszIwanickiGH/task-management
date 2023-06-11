b64774eedd7068632ce131f04c66ca2ab4240a0b
Tomasz <tomasz.iwanicki@onet.pl>
2023-06-11 16:00:44 +0200

    Change the way the title is displayed so that it changes dynamically

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Header = () => {
+  const { name } = useSelector((state) => state.boardName)
+
   return (
     <section className="p-6 bg-darkGray flex justify-between items-center w-full outline-1 outline-gray outline">
-      <h2 className="text-white text-[24px]">Platform Launch</h2>
+      <h2 className="text-white text-[24px]">{name}</h2>
       <div className="flex gap-6 items-center">
         <button className="bg-mainPurple py-2 w-[140px] rounded-full text-white">+ Add Task</button>
         <img src="assets/icon-vertical-ellipsis.svg" alt="" />
