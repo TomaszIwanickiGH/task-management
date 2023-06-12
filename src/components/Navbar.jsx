b0273042c294af49eeaebd4ca4df841e9393fe50
Tomasz <tomasz.iwanicki@onet.pl>
2023-06-12 17:55:52 +0200

    Add styles on hover

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import data from '../utils/data.json'
 import { useDispatch, useSelector } from 'react-redux'
 import { setBoardName } from '../redux/boardName'
@@ -7,29 +7,38 @@ const Navbar = () => {
   // const { name } = useSelector((state) => state.boardName)
   const dispatch = useDispatch()
 
+  const [isActive, setIsActive] = useState('Platform Launch')
+
   return (
-    <nav className="p-6 bg-darkGray flex flex-col w-[18%] gap-[45vh]">
-      <div className="flex flex-col gap-12 ">
-        <div>
+    <nav className="bg-darkGray flex flex-col w-[18%] gap-[45vh]">
+      <div className="flex flex-col gap-12 pt-2">
+        <div className="p-6 hover:cursor-pointer">
           <img src="assets/logo-light.svg" alt="logo" onClick={() => dispatch(setBoardName('Platform Launch'))} />
         </div>
         <div className="text-mediumGray">
-          <h3 className="font-bold text-[13px] tracking-[3px]">ALL BOARDS ({data.boards.length})</h3>
+          <h3 className="font-bold text-[13px] tracking-[3px] px-6">ALL BOARDS ({data.boards.length})</h3>
           <ul className="flex flex-col gap-4 mt-8">
             {data.boards.map((board) => (
-              <li key={board.name} className="flex gap-4 items-center" onClick={() => dispatch(setBoardName(board.name))}>
+              <li
+                key={board.name}
+                className={`px-6 py-2 flex gap-4 items-center ${isActive === board.name ? 'bg-mainPurple text-white' : 'text-mediumGray hover:bg-lightPurple'} w-[90%] hover:cursor-pointer hover:text-white  rounded-se-2xl rounded-ee-2xl`}
+                onClick={() => {
+                  dispatch(setBoardName(board.name))
+                  setIsActive(board.name)
+                }}
+              >
                 <img src="assets/icon-board.svg" alt="icon" />
                 {board.name}
               </li>
             ))}
-            <li className="text-mainPurple flex gap-4 items-center">
+            <li className="text-mainPurple hover:text-lightPurple flex gap-4 items-center px-6 hover:cursor-pointer">
               <img src="assets/icon-board-purple.svg" alt="icon" />
               +Create New Board
             </li>
           </ul>
         </div>
       </div>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 px-6">
         <div className="bg-veryDarkGray flex gap-4 justify-center items-center py-4 rounded-md">
           <img src="assets/icon-light-theme.svg" alt="icon" />
           <label className="switch">
