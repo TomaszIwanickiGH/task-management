b1d509bc99dc0fb5838f5eb5b6c4d0caf03ba93e
Tomasz <tomasz.iwanicki@onet.pl>
2023-06-17 19:08:06 +0200

    Hide the header on mobile devices

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   const { name } = useSelector((state) => state.boardName)
 
   return (
-    <section className="p-6 bg-darkGray flex justify-between items-center w-full outline-1 outline-gray outline">
+    <section className="p-6 bg-darkGray md:flex hidden justify-between items-center w-full outline-1 outline-gray outline">
       <h2 className="text-white text-[24px]">{name}</h2>
       <div className="flex gap-6 items-center">
         <button className="bg-mainPurple py-2 w-[140px] rounded-full text-white">+ Add Task</button>
