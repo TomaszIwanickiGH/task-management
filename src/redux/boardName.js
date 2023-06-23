eea128d74ea00cc1800458fd4721da0bb95cd4a8
Tomasz <tomasz.iwanicki@onet.pl>
2023-06-23 18:38:51 +0200

    Add some more global states

--- src/redux/boardName.js
+++ src/redux/boardName.js
@@ -4,14 +4,21 @@ export const boardNameSlice = createSlice({
   name: 'boardnName',
   initialState: {
     name: 'Platform Launch',
+    isClosed: false,
   },
   reducers: {
     setBoardName: (state, action) => {
       state.name = action.payload
     },
+    openTask: (state) => {
+      state.isClosed = true
+    },
+    closeTask: (state) => {
+      state.isClosed = false
+    },
   },
 })
 
-export const { setBoardName } = boardNameSlice.actions
+export const { setBoardName, openTask, closeTask } = boardNameSlice.actions
 
 export default boardNameSlice.reducer
